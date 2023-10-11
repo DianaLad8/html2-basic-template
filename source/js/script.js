9316562f6d64c9d0335b001a5afdcfccdf1d256d
DianaPavlova <diana50888@gmail.com>
2023-10-11 08:44:06 +0200

    Добавляет стилизацию шапки.

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const headerNav = document.querySelector(`.main-nav__page`);
+const headerToggle = headerNav.querySelector(`.main-nav__toogle`);
+
+headerToggle.addEventListener(`click`, () => {
+  headerNav.classList.toggle(`main-nav__page--opened`)
+  headerToggle.classList.toggle(`main-nav__page--opened`)
+});
