52fd755fa86b70504cc6f481c2e902d147a2cd36
DianaPavlova <diana50888@gmail.com>
2023-10-15 10:17:53 +0200

    Добавляет блок преимущест и рефракторинг скрипта.

--- source/js/script.js
+++ source/js/script.js
@@ -5,47 +5,49 @@
 const headerNav = document.querySelector(`.main-nav__page`);
 const headerToggle = headerNav.querySelector(`.main-nav__toogle`);
 
-headerToggle.addEventListener(`click`, () => {
-  headerNav.classList.toggle(`main-nav__page--opened`)
-  headerToggle.classList.toggle(`main-nav__page--opened`);
-});
+const onBurgerClick = () => {
+  headerToggle.addEventListener(`click`, () => {
+    headerNav.classList.toggle(`main-nav__page--opened`)
+    headerToggle.classList.toggle(`main-nav__page--opened`);
+  });
+};
+
+onBurgerClick();
+
 
 /* Слайдер */
 
 const buttonPrev = document.querySelector('.slider-button-prev');
 const buttonNext = document.querySelector('.slider-button-next');
-
-
 const slides = document.querySelectorAll('.slider__item');
 const steps = document.querySelectorAll('.slider__step');
-const slideCount = slides.length;
-const stopToggle = slideCount - 1;
+const stopIndex = slides.length - 1;
 
 let currentIndex = 0;
 
-buttonPrev.addEventListener('click', () => {
+function resetSlider () {
   const currentSlaid = document.querySelector('.slider__item--current');
   currentSlaid.classList.remove('slider__item--current');
   const currentStep = document.querySelector('.slider__step--current');
   currentStep.classList.remove('slider__step--current');
+ }
+
+buttonPrev.addEventListener('click', () => {
+  resetSlider();
    currentIndex --;
    if (currentIndex === 0) {
      buttonPrev.setAttribute("disabled", "disabled");
-   } else if (currentIndex < stopToggle) {
+   } else if (currentIndex < stopIndex) {
      buttonNext.removeAttribute("disabled", "disabled");
    }
    slides[currentIndex].classList.add('slider__item--current');
    steps[currentIndex].classList.add('slider__step--current');
  });
 
-
 buttonNext.addEventListener('click', () => {
-  const currentSlaid = document.querySelector('.slider__item--current');
-  currentSlaid.classList.remove('slider__item--current');
-  const currentStep = document.querySelector('.slider__step--current');
-  currentStep.classList.remove('slider__step--current');
+  resetSlider();
   currentIndex ++;
-  if (currentIndex === stopToggle) {
+  if (currentIndex === stopIndex) {
     buttonNext.setAttribute("disabled", "disabled");
   } else if (currentIndex > 0) {
     buttonPrev.removeAttribute("disabled", "disabled");
@@ -58,10 +60,7 @@ buttonNext.addEventListener('click', () => {
 
 steps.forEach((step, i) => {
   step.addEventListener('click', () => {
-   const currentSlaid = document.querySelector('.slider__item--current');
-   currentSlaid.classList.remove('slider__item--current');
-   const currentStep = document.querySelector('.slider__step--current');
-  currentStep.classList.remove('slider__step--current');
+  resetSlider();
   currentIndex = i;
   if (currentIndex === 0 && buttonPrev.hasAttribute('disable') && buttonNext.hasAttribute('disabled') ) {
     buttonNext.removeAttribute("disabled", "disabled");
@@ -69,9 +68,9 @@ steps.forEach((step, i) => {
     buttonPrev.setAttribute("disabled", "disabled");
   } else if (0 < currentIndex && buttonPrev.hasAttribute('disabled')) {
     buttonPrev.removeAttribute('disabled');
-  } else if (currentIndex < stopToggle && buttonNext.hasAttribute('disabled')) {
+  } else if (currentIndex < stopIndex && buttonNext.hasAttribute('disabled')) {
     buttonNext.removeAttribute('disabled');
-  } else if (currentIndex === stopToggle && !buttonNext.hasAttribute('disabled')) {
+  } else if (currentIndex === stopIndex && !buttonNext.hasAttribute('disabled')) {
     buttonNext.setAttribute("disabled", "disabled");
   }
     steps[currentIndex].classList.add('slider__step--current');
